0248e214c6a1bfd4d6642d489a6af47b0aa17e4b
= <=>
2025-10-13 10:19:23 +0100

    Tic Tac Toe Game

---
+++ script.js
@@ -0,0 +1,107 @@
+const cell = [
+    0, 1, 2, 
+    3, 4, 5, 
+    6, 7, 8
+];
+
+const symbols = {
+    x: `
+    <div class="x-symbol">
+        <svg viewBox="0 0 120 120" width="60" height="60">
+            <g fill="none" stroke="#4F837A" stroke-width="17" stroke-linecap="round" stroke-linejoin="round">
+            <path class="line1" d="M30 30 L90 90" />
+            <path class="line2" d="M90 30 L30 90" />
+      </g>
+    </svg>
+  </div>
+    `,
+    o:  `
+        <div class="o-symbol">
+    <svg viewBox="0 0 120 120" width="60" height="60">
+      <circle cx="60" cy="60" r="34" fill="none" stroke="#D97C67" stroke-width="17" stroke-linecap="round" />
+    </svg>
+  </div>
+    `
+}
+
+const gameCell = document.querySelectorAll(".empty");
+let currentPlayer = "x";
+
+for (let i = 0; i < gameCell.length; i++) {
+    gameCell[i].addEventListener('click', ()=>{
+        
+        if (gameCell[i].classList.contains("empty")) {
+            cell[i] = currentPlayer;   
+            displayAnimation(i, currentPlayer);
+            gameCell[i].classList.remove("empty");
+            gameCell[i].classList.add(currentPlayer);
+            isFirstPlayer = !isFirstPlayer;
+            pass();
+        }
+        changePlayer();
+        console.log(gameCell);
+        console.log(cell);
+
+    });   
+}
+
+const displayAnimation = (i, currentPlayer) => {
+    currentPlayer === 'x' ? gameCell[i].innerHTML = `${symbols.x}` : gameCell[i].innerHTML = `${symbols.o}`    
+}
+
+let isFirstPlayer = true;
+
+function changePlayer() {
+    if (isFirstPlayer) {
+        currentPlayer = "x";
+    } else {
+        currentPlayer = "o"
+    }
+}
+
+
+function pass(){
+
+    switch (true) {
+        // diagonal wins
+        case (cell[0] === currentPlayer && cell[4] === currentPlayer && cell[8] === currentPlayer): displayWinner(currentPlayer); break;
+        case (cell[2] === currentPlayer && cell[4] === currentPlayer && cell[6] === currentPlayer): displayWinner(currentPlayer); break;
+        
+        // vertical wins
+        case (cell[0] === currentPlayer && cell[3] === currentPlayer && cell[6] === currentPlayer): displayWinner(currentPlayer); break;
+        case (cell[1] === currentPlayer && cell[4] === currentPlayer && cell[7] === currentPlayer): displayWinner(currentPlayer); break; 
+        case (cell[2] === currentPlayer && cell[5] === currentPlayer && cell[8] === currentPlayer): displayWinner(currentPlayer); break; 
+        
+        // horizontal wins
+        case (cell[0] === currentPlayer && cell[1] === currentPlayer && cell[2] === currentPlayer): displayWinner(currentPlayer); break; 
+        case (cell[3] === currentPlayer && cell[4] === currentPlayer && cell[5] === currentPlayer): displayWinner(currentPlayer); break; 
+        case (cell[6] === currentPlayer && cell[7] === currentPlayer && cell[8] === currentPlayer): displayWinner(currentPlayer); break; 
+    }
+
+    displayDraw();
+}
+
+function displayWinner(currentPlayer) {
+    setTimeout(()=>{
+        if (currentPlayer === "x") {  
+            alert("Player 1 wins");
+            location.reload()
+        } else {
+            alert("Player 2 wins");
+            location.reload()
+        }
+    }, 500);
+}
+
+function displayDraw() {
+    // let x = document.querySelectorAll(".x");
+    // let o = document.querySelectorAll(".o");
+    // if (x.length === 4 && o.length === 5 || x.length === 5 && o.length === 4) {
+    //     alert("Draw!")
+    // }
+
+    // doesn't work
+}
+
+
+// BOT Commands
