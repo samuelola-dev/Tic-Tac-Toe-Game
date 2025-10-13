9f549bc1b309b9b9c6088802a276fb2a46155783
= <=>
2025-10-13 12:08:19 +0100

    Player vs Bot script added

---
+++ bot.js
@@ -0,0 +1,129 @@
+const cell = [
+    0, 1, 2, 
+    3, 4, 5, 
+    6, 7, 8
+];
+
+const symbols = {
+    x: `
+    <div class="x-symbol">
+        <svg viewBox="0 0 120 120" width="60" height="60">
+            <g fill="none" stroke="#4F837A" stroke-width="17" stroke-linecap="round" stroke-linejoin="round">
+            <path class="line1" d="M30 30 L90 90" />
+            <path class="line2" d="M90 30 L30 90" />
+      </g>
+    </svg>
+  </div>
+    `,
+    o:  `
+        <div class="o-symbol">
+    <svg viewBox="0 0 120 120" width="60" height="60">
+      <circle cx="60" cy="60" r="34" fill="none" stroke="#D97C67" stroke-width="17" stroke-linecap="round" />
+    </svg>
+  </div>
+    `
+}
+
+const gameCell = document.querySelectorAll(".empty");
+let currentPlayer = "x";
+
+for (let i = 0; i < gameCell.length; i++) {
+    gameCell[i].addEventListener('click', ()=>{
+        
+        if (gameCell[i].classList.contains("empty")) {
+            changePlayer();
+            cell[i] = currentPlayer;   
+            displayAnimation(i, currentPlayer);
+            gameCell[i].classList.remove("empty");
+            gameCell[i].classList.add(currentPlayer);
+            pass();
+            botChoice();
+        }
+        console.log(gameCell);
+        console.log(cell);
+        
+    });   
+}
+
+const displayAnimation = (i, currentPlayer) => {
+    currentPlayer === 'x' ? gameCell[i].innerHTML = `${symbols.x}` : gameCell[i].innerHTML = `${symbols.o}`    
+}
+
+let isFirstPlayer = true;
+
+function botChoice() {
+    isFirstPlayer = !isFirstPlayer;
+    changePlayer();
+    const empty = document.querySelectorAll(".empty");
+    const emptyCell = cell.filter((cell)=>{
+        return isFinite(cell)
+    });
+
+    console.log("Bot is " + currentPlayer)
+    console.log(emptyCell);
+    let randomCell = emptyCell[Math.floor(Math.random() * emptyCell.length)];
+    console.log(randomCell + " is choosen");
+    console.log(gameCell[randomCell]);
+
+    if (isFinite(randomCell)) {
+        cell[randomCell] = currentPlayer;
+        gameCell[randomCell].classList.remove('empty');
+        gameCell[randomCell].classList.add(currentPlayer);
+        setTimeout(()=> {
+            displayAnimation(randomCell, currentPlayer);
+        }, 900)
+    } else {
+        console.log("Ai can't choose again");
+    }
+
+    pass();
+    isFirstPlayer = !isFirstPlayer;
+
+}
+
+
+
+
+function changePlayer() {
+    if (isFirstPlayer) {
+        currentPlayer = "x";
+    } else {
+        currentPlayer = "o"
+    }
+}
+
+
+function pass(){
+
+    switch (true) {
+        // diagonal wins
+        case (cell[0] === currentPlayer && cell[4] === currentPlayer && cell[8] === currentPlayer): displayWinner(currentPlayer); break;
+        case (cell[2] === currentPlayer && cell[4] === currentPlayer && cell[6] === currentPlayer): displayWinner(currentPlayer); break;
+        
+        // vertical wins
+        case (cell[0] === currentPlayer && cell[3] === currentPlayer && cell[6] === currentPlayer): displayWinner(currentPlayer); break;
+        case (cell[1] === currentPlayer && cell[4] === currentPlayer && cell[7] === currentPlayer): displayWinner(currentPlayer); break; 
+        case (cell[2] === currentPlayer && cell[5] === currentPlayer && cell[8] === currentPlayer): displayWinner(currentPlayer); break; 
+        
+        // horizontal wins
+        case (cell[0] === currentPlayer && cell[1] === currentPlayer && cell[2] === currentPlayer): displayWinner(currentPlayer); break; 
+        case (cell[3] === currentPlayer && cell[4] === currentPlayer && cell[5] === currentPlayer): displayWinner(currentPlayer); break; 
+        case (cell[6] === currentPlayer && cell[7] === currentPlayer && cell[8] === currentPlayer): displayWinner(currentPlayer); break; 
+    }
+
+    // displayDraw();
+}
+
+function displayWinner(currentPlayer) {
+    if (currentPlayer === "x") {  
+            setTimeout(()=>{
+                alert("Player 1 wins");
+                location.reload();
+            }, 500);
+        } else {
+             setTimeout(()=>{
+                alert("Bot wins");
+            }, 1000);
+        }
+}
+
