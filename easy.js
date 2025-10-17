06d9c8773442734609729261bd6a12ef74d1602a
Samuel Ola <samuelola396@gmail.com>
2025-10-17 22:47:27 +0100

    easy level updated

--- easy.js
+++ easy.js
@@ -13,108 +13,41 @@ function botChoice() {
     isFirstPlayer = !isFirstPlayer;
 }
 
-
-
 // Easy Mode
 
 function easyMode(){
-    // Bot chooses to block or not block (2 out of 3 times)
-    const win = [false, false, true, false]
-    const winDecision = win[Math.floor(Math.random() * win.length)]
-    botBlock();
+    botWin()
     pass();
 }
 
 
+// Easy Wining Chances
+function botCanWin(winCell){
+    const win = [false, false, true, false]
+    const winDecision = win[Math.floor(Math.random() * win.length)]
 
-// If can bot block
+    if (winDecision) {
+        console.log("Bot can win and Allowed");
+        botTurn(winCell);
+    } else {
+        console.log("Bot can win and Not Allowed");
+        botBlock();
+    }
+}
 
+// Easy Block Chances
 function botCanBlock(blockedCell){
     const block = [false, true, false];
     const blockDecision = block[Math.floor(Math.random() * block.length)];
 
     if (blockDecision){
-        console.log("Decision True!");
+        console.log("Bot can Block and Allowed");
         botTurn(blockedCell);
     } else {
-        console.log("Play on, bot can block but didn't meet decision")
+        console.log("Bot can Block and Not allowed")
         botRandom();
     }
 }
 
 
 
-function botBlock() {
-    console.log("Now working since there is no wins")
-    switch (true) {
-        // diagonal block
-        case (cell[0] !== bot && cell[4] === firstPlayer && cell[8] === firstPlayer): botCanBlock(0); break;
-        case (cell[0] === firstPlayer && cell[4] !== bot && cell[8] === firstPlayer): botCanBlock(4); break;
-        case (cell[0] === firstPlayer && cell[4] === firstPlayer && cell[8] !== bot): botCanBlock(8); break;
-        
-        case (cell[2] !== bot && cell[4] === firstPlayer && cell[6] === firstPlayer): botCanBlock(2); break;
-        case (cell[2] === firstPlayer && cell[4] !== bot && cell[6] === firstPlayer): botCanBlock(4); break;
-        case (cell[2] === firstPlayer && cell[4] === firstPlayer && cell[6] !== bot): botCanBlock(6); break;
-        
-
-        // vertical block 
-        case (cell[0] !== bot && cell[3] === firstPlayer && cell[6] === firstPlayer): botCanBlock(0); break;
-        case (cell[0] === firstPlayer && cell[3] !== bot && cell[6] === firstPlayer): botCanBlock(3); break;
-        case (cell[0] === firstPlayer && cell[3] === firstPlayer && cell[6] !== bot): botCanBlock(6); break;
-
-
-        case (cell[1] !== bot && cell[4] === firstPlayer && cell[7] === firstPlayer): botCanBlock(1); break;
-        case (cell[1] === firstPlayer && cell[4] !== bot && cell[7] === firstPlayer): botCanBlock(4); break;
-        case (cell[1] === firstPlayer && cell[4] === firstPlayer && cell[7] !== bot): botCanBlock(7); break;
-
-
-        case (cell[2] !== bot && cell[5] === firstPlayer && cell[8] === firstPlayer): botCanBlock(2); break;
-        case (cell[2] === firstPlayer && cell[5] !== bot && cell[8] === firstPlayer): botCanBlock(5); break;
-        case (cell[2] === firstPlayer && cell[5] === firstPlayer && cell[8] !== bot): botCanBlock(8); break;
-
-        // horizontal block
-        case (cell[0] !== bot && cell[1] === firstPlayer && cell[2] === firstPlayer): botCanBlock(0); break;
-        case (cell[0] === firstPlayer && cell[1] !== bot && cell[2] === firstPlayer): botCanBlock(1); break;
-        case (cell[0] === firstPlayer && cell[1] === firstPlayer && cell[2] !== bot): botCanBlock(2); break;
-
-        case (cell[3] !== bot && cell[4] === firstPlayer && cell[5] === firstPlayer): botCanBlock(3); break;
-        case (cell[3] === firstPlayer && cell[4] !== bot && cell[5] === firstPlayer): botCanBlock(4); break;
-        case (cell[3] === firstPlayer && cell[4] === firstPlayer && cell[5] !== bot): botCanBlock(5); break;
-
-
-        case (cell[6] !== bot && cell[7] === firstPlayer && cell[8] === firstPlayer): botCanBlock(6); break;
-        case (cell[6] === firstPlayer && cell[7] !== bot && cell[8] === firstPlayer): botCanBlock(7); break;
-        case (cell[6] === firstPlayer && cell[7] === firstPlayer && cell[8] !== bot): botCanBlock(8); break;
-        default: botRandom(); break;
-    }
-}
-
-function botWin(){
-    switch (true) {
-        case (cell[4] === bot && cell[8] === bot): botTurn(0); break;
-        case (cell[0] === bot && cell[8] === bot): botTurn(4); break;
-        case (cell[0] === bot && cell[4] === bot): botTurn(8); break;   
-        case (cell[4] === bot && cell[6] === bot): botTurn(2); break;
-        case (cell[2] === bot && cell[6] === bot): botTurn(4); break;
-        case (cell[2] === bot && cell[4] === bot): botTurn(6); break;
-        case (cell[3] === bot && cell[6] === bot): botTurn(0); break;
-        case (cell[0] === bot && cell[6] === bot): botTurn(3); break;
-        case (cell[0] === bot && cell[3] === bot): botTurn(6); break;
-        case (cell[4] === bot && cell[7] === bot): botTurn(1); break;
-        case (cell[1] === bot && cell[7] === bot): botTurn(4); break;
-        case (cell[1] === bot && cell[4] === bot): botTurn(7); break;
-        case (cell[5] === bot && cell[8] === bot): botTurn(2); break;
-        case (cell[2] === bot && cell[8] === bot): botTurn(5); break;
-        case (cell[2] === bot && cell[5] === bot): botTurn(8); break;
-        case (cell[1] === bot && cell[2] === bot): botTurn(0); break;
-        case (cell[0] === bot && cell[2] === bot): botTurn(1); break;
-        case (cell[0] === bot && cell[1] === bot): botTurn(2); break;
-        case (cell[4] === bot && cell[5] === bot): botTurn(3); break;
-        case (cell[3] === bot && cell[5] === bot): botTurn(4); break;
-        case (cell[3] === bot && cell[4] === bot): botTurn(5); break;
-        case (cell[7] === bot && cell[8] === bot): botTurn(6); break;
-        case (cell[6] === bot && cell[8] === bot): botTurn(7); break;
-        case (cell[6] === bot && cell[7] === bot): botTurn(8); break;
-        default: botBlock(); break;
-    }
-}
